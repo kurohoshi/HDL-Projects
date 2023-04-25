b237580aee88e9d13705e51dd8052b8f9ebfa34a
Lingxing Zheng <xingling39@gmail.com>
2023-04-24 22:16:06 -0500

    Major logic overhaul
     - renaming en port into init to clarify the functionality of the port
     - reimplementing sclk generation
     - making several signals (sclk_pulse, r_sel, o_mosi) from next state logic to combinational to avoid timing issues
     - fix mosi and miso logic flow
     - received Rx data is sent to output  when the transmission finishes (any sooner will cause the last bit to have problems)

--- spi/spi.srcs/sources_1/new/spi_master.vhd
+++ spi/spi.srcs/sources_1/new/spi_master.vhd
@@ -33,17 +33,18 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity spi_master is
   Generic(
+    DATA_BITS : INTEGER := 8;
     CLKS_PER_HALF_SCLK: INTEGER := 2
   );
   Port(
     i_clk    : in  STD_LOGIC;
-    i_en     : in  STD_LOGIC;
+    i_init : in  STD_LOGIC;
     i_reset  : in  STD_LOGIC;
     
     i_mode   : in STD_LOGIC_VECTOR(1 downto 0);
     
     i_tx_data : in STD_LOGIC_VECTOR(7 downto 0);
-    o_tx_busy : out STD_LOGIC;
+    o_sel : out STD_LOGIC;
     
     o_rx_data : out STD_LOGIC_VECTOR(7 downto 0);
     
@@ -59,11 +60,13 @@ architecture Behavioral of spi_master is
   signal r_sclk : STD_LOGIC;
   signal r_en   : STD_LOGIC;
   signal en_pulse : STD_LOGIC;
+  signal r_sel  : STD_LOGIC;
+  signal r_sclk_edge_counter : INTEGER range 0 to DATA_BITS*2+1;
   
   signal r_tx_data : STD_LOGIC_VECTOR(i_tx_data'range);
   signal r_rx_data : STD_LOGIC_VECTOR(o_rx_data'range);
-  signal r_tx_bit_counter : INTEGER range 0 to 7;
-  signal r_rx_bit_counter : INTEGER range 0 to 7;
+  signal r_tx_bit_counter : INTEGER range 0 to DATA_BITS-1;
+  signal r_rx_bit_counter : INTEGER range 0 to DATA_BITS-1;
   
   signal sclk_pulse : STD_LOGIC;
   signal mosi_pulse : STD_LOGIC;
@@ -76,67 +79,94 @@ begin
   begin
     if(i_reset = '1') then
       r_sclk <= cpol;
-      sclk_pulse <= '0';
       sclk_counter <= 0;
+      r_sclk_edge_counter <= 0;
     elsif(rising_edge(i_clk)) then
-      if(i_en = '1') then
-        if(sclk_counter = CLKS_PER_HALF_SCLK-1) then
-          sclk_counter <= 0;
-          r_sclk <= not r_sclk;
-          sclk_pulse <= '1';
-        else
-          sclk_counter <= sclk_counter + 1;
-          sclk_pulse <= '0';
+      sclk_counter <= 0;
+
+      if(en_pulse = '1') then
+        r_sclk <= cpol;
+        sclk_counter <= CLKS_PER_HALF_SCLK-1;
+        r_sclk_edge_counter <= DATA_BITS*2+1;
+      else
+        if(r_sclk_edge_counter /= 0) then
+          if(sclk_counter = 0) then
+            if(r_sclk_edge_counter > 1) then
+              r_sclk <= not r_sclk;
+              sclk_counter <= CLKS_PER_HALF_SCLK-1;
+            end if;
+            
+            r_sclk_edge_counter <= r_sclk_edge_counter - 1;
+          else
+            sclk_counter <= sclk_counter - 1;
+          end if;
         end if;
       end if;
     end if;
   end process;
-  
+
+  r_sel <= '1' when r_sclk_edge_counter /= 0 else '0';
+  sclk_pulse <= '1' when sclk_counter = 0 and r_sclk_edge_counter > 1 else '0';
   mosi_pulse <= sclk_pulse and (r_sclk xor (i_mode(1) xor i_mode(0)));
   miso_pulse <= sclk_pulse and not (r_sclk xor (i_mode(1) xor i_mode(0)));
   
   data_reg: process(i_clk)
   begin
     if(rising_edge(i_clk)) then
-      r_en <= i_en;
+      r_en <= i_init;
       
-      if(i_en = '1' and r_en = '0') then
+      if(i_init = '1' and r_en = '0') then
+        en_pulse <= '1';
         r_tx_data <= i_tx_data;
+      else
+        en_pulse <= '0';
       end if;
     end if;
   end process;
-  
-  en_pulse <= i_en and not r_en;
   
   MOSI_serialize: process(i_clk)
   begin
     if(rising_edge(i_clk)) then
-      if(i_en = '1') then -- ????????
-        if(en_pulse = '1' and cpha = '0') then
-          o_mosi <= r_tx_data(7);
-          r_tx_bit_counter <= 6;
-        elsif(mosi_pulse = '1') then
-          o_mosi <= r_tx_data(r_tx_bit_counter);
-          r_tx_bit_counter <= r_tx_bit_counter - 1;
+        if(en_pulse = '1') then
+          r_tx_bit_counter <= 7;
+        end if;
+
+        if((en_pulse = '1' and cpha = '0') or mosi_pulse = '1') then
+          if(not (cpha = '1' and r_sclk_edge_counter = DATA_BITS*2+1) and r_tx_bit_counter /= 0) then
+            r_tx_bit_counter <= r_tx_bit_counter - 1;
+          end if;
+        end if;
+    end if;
+  end process;
+
+  o_mosi <= r_tx_data(r_tx_bit_counter) when r_sel = '1' else '0';
+  
+  MISO_read: process(i_clk)
+  begin
+    if(rising_edge(i_clk)) then
+      if(en_pulse = '1') then
+        r_rx_bit_counter <= 7;
+      end if;
+
+      if(miso_pulse = '1' ) then
+        r_rx_data(r_rx_bit_counter) <= i_miso;
+        
+        if(r_rx_bit_counter /= 0) then
+          r_rx_bit_counter <= r_rx_bit_counter - 1;
         end if;
       end if;
     end if;
   end process;
   
-  MISO_read: process(i_clk)
+  rx_data_out: process(i_clk)
   begin
     if(rising_edge(i_clk)) then
-      if(i_en = '1') then
-        if(en_pulse = '1') then
-          r_rx_bit_counter <= 7;
-        elsif(miso_pulse = '1' ) then
-          r_rx_data(r_rx_bit_counter) <= i_miso;
-          r_rx_bit_counter <= r_rx_bit_counter - 1;
-          if(r_rx_bit_counter = 0) then
-            o_rx_data <= r_rx_data;
-          end if;
-        end if;
-      end if;
+      if(sclk_counter = 0 and r_sclk_edge_counter = 1) then
+        o_rx_data <= r_rx_data;
+      end if;  
     end if;
   end process;
+
+  o_sclk <= r_sclk;
+  o_sel  <= r_sel;
 end Behavioral;
